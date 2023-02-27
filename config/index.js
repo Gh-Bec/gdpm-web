85d1cdc2586d25ce996b4f3007c7c86aa431f977
LinTa0 <ltao0518@aliyun.com>
2023-02-27 22:45:47 +0800

    修改启动时的默认IP

--- config/index.js
+++ config/index.js
@@ -13,7 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '192.168.74.1', // can be overwritten by process.env.HOST
+    // 修改启动时的默认IP
+    host: '127.0.0.1', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
